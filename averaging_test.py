a2af48a6bb290e26c2efe27d99305d759236d520
Nate Seifert <nseifert@newhaven.edu>
2025-04-02 13:18:39 -0400

    blah

--- averaging_test.py
+++ averaging_test.py
@@ -4,19 +4,19 @@ from matplotlib import pyplot as plt
 import numpy as np 
 
 if __name__ == "__main__":
-    NUM_AVERAGES = 10
+    NUM_AVERAGES = 1
 
     # Set up digitizer
     dig = Pitaya(host='192.168.0.2', name='Pitaya',
                  timeout=5.0,
                  two_channel= True,
                  trig_lvl = 0.25,
-                 data_size = 5000,
-                 acq_len = 5000,)
+                 data_size = 15000,
+                 acq_len = 15000,)
     
     dig.ext_clock() # Enable external clock
     # Enable trigger source on Out1 --> CH2 Trig
-    dig.set_synth(freq=1.0E4, amp=1.0, channel=0,type='PWM')
+    dig.set_synth(freq=30E4, amp=1.0, channel=0,type='SINE')
 
     print(f'Building accumulation array of dimensions {dig.data_size} x {NUM_AVERAGES}')
     accumulated = np.zeros((int(dig.data_size), NUM_AVERAGES))
@@ -27,10 +27,12 @@ if __name__ == "__main__":
     averaged = np.mean(accumulated,axis=1)
     fig, ax = plt.subplots(2)
     for i in range(NUM_AVERAGES):
-
-        ax[0].plot(accumulated[:,i]+np.max(accumulated[:,i]),label=f'Shot {i}')
+        ax[0].plot(accumulated[:,i],label=f'Shot {i}')
+        ax[0].scatter(x=np.arange(0,len(accumulated[:,i]),1),y=accumulated[:,i],marker='x',color='k',s=5)
     ax[1].plot(averaged, 'r', label='Average acq')
     
     plt.show()
+    np.savetxt('Spec_OFFRES_10GHz.txt',averaged)
+
 
         
