c3b50b6f9505912aca2a081d180213291dc3e33a
Nate Seifert <nseifert@newhaven.edu>
2025-04-04 10:50:23 -0400

    added cavity measurements

--- averaging_test.py
+++ averaging_test.py
@@ -2,8 +2,10 @@ from digitizer import Pitaya
 import time
 from matplotlib import pyplot as plt
 import numpy as np 
+from scipy.fftpack import fft, fftfreq
 
 if __name__ == "__main__":
+    FREQ = 14 # GHz
     NUM_AVERAGES = 1
 
     # Set up digitizer
@@ -25,14 +27,23 @@ if __name__ == "__main__":
         print(f'Acquired {i+1}/{NUM_AVERAGES} acquistions')
     
     averaged = np.mean(accumulated,axis=1)
-    fig, ax = plt.subplots(2)
+
+    # Calc fft
+    out_fft = np.abs(fft(averaged))
+    out_fft_freq = fftfreq(len(averaged),d=1/250E6)
+    fft_mask = out_fft_freq > 0
+
+    fig, ax = plt.subplots(3)
     for i in range(NUM_AVERAGES):
         ax[0].plot(accumulated[:,i],label=f'Shot {i}')
         ax[0].scatter(x=np.arange(0,len(accumulated[:,i]),1),y=accumulated[:,i],marker='x',color='k',s=5)
     ax[1].plot(averaged, 'r', label='Average acq')
+    ax[2].plot(out_fft_freq[fft_mask]/1E6, out_fft[fft_mask])
+    ax[2].set_xlim([25,35])
     
     plt.show()
-    np.savetxt('Spec_OFFRES_10GHz.txt',averaged)
+
+    np.savetxt(f'Spec_OFFRES_{FREQ}GHz.txt',averaged)
 
 
         
